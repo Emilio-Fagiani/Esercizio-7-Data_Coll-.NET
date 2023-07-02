6c2d17ab85b62990b8e843299b7f314cc627788b
emifa <emifa@LAPTOP-1TM449QB>
2023-07-02 14:05:24 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-7-Data_Coll-.NET/Program.cs
@@ -0,0 +1,16 @@
+﻿
+HashSet<string> strings = new HashSet<string>();
+strings.Add("Hello");
+strings.Add("Hello");
+strings.Add("Word");
+strings.Add("Ciao");
+strings.Add("Mondo");
+strings.Add("mondo");
+strings.Add("ciao");
+strings.Add("Ciao");
+
+foreach (string s in strings)  
+    Console.WriteLine(s);   
+
+//Come si può notare l'hashset considera uniche le stringhe che sono uguali lettera per lettera
+//e facendo distizione fra le parole con l'iniziale maiuscola, considerandole quindi come stringhe diverse.
